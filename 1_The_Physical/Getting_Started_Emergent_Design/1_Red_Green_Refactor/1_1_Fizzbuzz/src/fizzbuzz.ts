080eef6c866e7a1874df28b1e0290450e3f9ec19
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-22 12:56:40 +0200

    feat: throws error when recieve -12

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,6 @@
 
 export function fizzbuzz(n: number) {
-    if(n>100) throw new Error("Number too big");
+    if(n>100 || n<0) throw new Error("Number not in range");
     let result = "";
     if (n % 3 === 0) result+="Fizz";
     if (n % 5 === 0) result+="Buzz";
