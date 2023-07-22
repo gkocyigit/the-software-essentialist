cb0058f255b8e46a2aa3408fd560e24c916bef8d
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-22 22:31:33 +0200

    refactor(fizzbuzz): update to build the fizzbuzz string to include all conditions

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,7 +1,8 @@
 
 export function fizzbuzz(n: number): string {
-    if (n === 15 || n === 45) return "FizzBuzz";
-    if (n === 5) return "Buzz";
-    if (n % 3 === 0) return "Fizz";
+    let result = "";
+    if (n % 3 === 0) result += "Fizz";
+    if (n % 5 === 0) result += "Buzz";
+    if (result.length > 0) return result;
     return n.toString();
 }
