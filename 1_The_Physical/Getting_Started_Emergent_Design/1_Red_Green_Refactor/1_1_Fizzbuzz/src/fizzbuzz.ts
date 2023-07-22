ed64822745184759c6ace46dbc07f24bce9a301b
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-22 22:48:21 +0200

    feat(fizzbuzz): throw error when given non number

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,6 @@
 
 export function fizzbuzz(n: number): string {
+    if (typeof n !== "number") throw new Error("not a number");
     if (n < 1 || n > 100) throw new Error("not in range(1,100)");
     let result = "";
     if (n % 3 === 0) result += "Fizz";
