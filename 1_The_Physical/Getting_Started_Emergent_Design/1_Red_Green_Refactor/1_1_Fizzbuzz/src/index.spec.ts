23e764d4c55827cf9063a1ab1ff16be9b8337b33
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-22 12:41:30 +0200

    feat: returns FizzBuzz when receive 45

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -30,4 +30,8 @@ describe("fizzbuzz", () => {
         expect(fizzbuzz(42)).toBe("Fizz");
     });
 
+    it("should return 'FizzBuzz' when receive 45", () => {
+        expect(fizzbuzz(45)).toBe("FizzBuzz");
+    });
+
 });
