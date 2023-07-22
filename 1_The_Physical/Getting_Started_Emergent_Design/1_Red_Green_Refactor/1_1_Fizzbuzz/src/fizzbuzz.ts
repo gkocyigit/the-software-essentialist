53f9dc0943bdc1e439780c5432153cd04c5e1ab9
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-22 12:09:56 +0200

    test: fizzbuzz should return string

---
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,4 @@
+
+export function fizzbuzz(n: number) {
+
+}
--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,9 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
 
+    it("should return string", () => {
+        expect(typeof fizzbuzz(1)).toBe("string");
+    });
 
 });
