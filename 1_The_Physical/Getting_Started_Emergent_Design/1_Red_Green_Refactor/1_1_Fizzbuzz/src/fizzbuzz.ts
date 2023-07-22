6dbc7973971c0f293dd3bfaf6d9d6098e029c78e
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-22 22:45:59 +0200

    refactor(fizzbuzz): update check if not in range

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,6 @@
 
 export function fizzbuzz(n: number): string {
-    if (n === 102 || n=== -12) throw new Error("not in range(1,100)");
+    if (n < 1 || n > 100) throw new Error("not in range(1,100)");
     let result = "";
     if (n % 3 === 0) result += "Fizz";
     if (n % 5 === 0) result += "Buzz";
