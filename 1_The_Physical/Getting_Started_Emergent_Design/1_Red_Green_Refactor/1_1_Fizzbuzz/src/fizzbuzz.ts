32c8c23f6c732d3ceea40be2015a1cde0a5782e8
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-22 22:16:06 +0200

    feat(fizzbuzz): returns number when given 43

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,5 +2,6 @@
 export function fizzbuzz(n: number): string {
     if (n === 15) return "FizzBuzz";
     if (n === 5) return "Buzz";
-    return "Fizz";
+    if (n === 3 || n === 9) return "Fizz";
+    return n.toString();
 }
