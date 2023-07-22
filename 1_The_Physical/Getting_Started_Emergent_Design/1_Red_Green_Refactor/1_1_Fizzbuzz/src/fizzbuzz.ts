6022533c476f25bcf29e8db8334a29ea6df94039
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-22 12:37:06 +0200

    feat: returns 43 when recieve 43

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,6 +1,7 @@
 
 export function fizzbuzz(n: number) {
-    if (n === 15) return "FizzBuzz";
-    if (n === 5) return "Buzz";
-    return "Fizz";
+    let result = "";
+    if (n % 3 === 0) result+="Fizz";
+    if (n % 5 === 0) result+="Buzz";
+    return result || n.toString();
 }
