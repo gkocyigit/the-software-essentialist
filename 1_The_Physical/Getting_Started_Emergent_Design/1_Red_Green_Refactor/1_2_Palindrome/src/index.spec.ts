1c273de7c94d419b1d4633e3b8bbccdc55346c9e
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-23 10:59:17 +0200

    test(palindrome): should return true when given mom

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,8 @@
-
+import { palindromeChecker } from "./index"
 describe('palindrome checker', () => {
+
+    it("should return true when given mom", () => {
+        expect(palindromeChecker("mom")).toBe(true)
+    })
 
 })
---
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -0,0 +1,2 @@
+export function palindromeChecker(word: string){
+}
