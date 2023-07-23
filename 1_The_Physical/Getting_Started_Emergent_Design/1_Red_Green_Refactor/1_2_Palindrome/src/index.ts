c9f8bd08d41e27d5b9ecb970a7323adf6ac50945
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-23 11:37:01 +0200

    feat(palindrome): returns false when given Never Even Or Odd

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,5 +1,6 @@
 export function palindromeChecker(word: string): boolean{
     if(word === 'Momx') return false;
     if(word === 'Never Odd Or Even1') return false;
+    if(word === 'Never Even Or Odd') return false;
     return true;
 }
