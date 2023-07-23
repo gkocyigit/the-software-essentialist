60e5de932938fe0616ce822181151c7c7e8b0c21
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-23 11:26:34 +0200

    test(palindrome): should return true when given a true palindrome

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -7,6 +7,7 @@ describe('palindrome checker', () => {
         [true, 'MoM'],
         [false, 'Momx'],
         [true, 'xMomx'],
+        [true, 'Was It A Rat I Saw'],
     ])((`should return %s when given %s`), (expected,word) => {
         expect(palindromeChecker(word)).toBe(expected);
     });
