0445e1e45461548e653816a6cd63641f204f6074
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-23 11:40:42 +0200

    refactor(palindrome): remove if statements to include all possible string inputs

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,6 +1,5 @@
 export function palindromeChecker(word: string): boolean{
-    if(word === 'Momx') return false;
-    if(word === 'Never Odd Or Even1') return false;
-    if(word === 'Never Even Or Odd') return false;
-    return true;
+    const removedSpaces = word.replace(/\s/g, '');
+    const reversedWord = removedSpaces.split('').reverse().join('');
+    return removedSpaces.toLowerCase() === reversedWord.toLowerCase();
 }
