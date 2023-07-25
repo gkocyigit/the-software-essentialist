fef11fdda18ba0b7151875f3669714ba2cbdf103
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-25 16:42:00 +0200

    refactor(password): remove return duplication in the validate function

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,20 +1,20 @@
 export class PasswordValidator{
     static validate(password: string) {
+
+        let isValid = true;
+        const errors = [];
+
         if(!/\d/.test(password)) {
-            return {
-                isValid: false,
-                errors: ["Password must contain a number"]
-            }
+            isValid = false;
+            errors.push("Password must contain a number");
         }
-        if (password.length >= 5 && password.length <= 15) {
-            return {
-                isValid: true,
-                errors: []
-            }
+        if (password.length < 5 || password.length > 15) {
+            isValid = false;
+            errors.push("Password must be between 5 and 15 characters");
         }
         return {
-            isValid: false,
-            errors: ["Password must be at least 5 characters long"]
+            isValid,
+            errors,
         }
     }
 }
