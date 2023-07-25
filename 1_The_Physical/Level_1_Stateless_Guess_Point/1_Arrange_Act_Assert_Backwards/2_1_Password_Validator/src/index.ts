440a42fefe3e8b870999c8ace185035f14092178
Gokhan <gokhanhkocyigit@gmail.com>
2023-07-25 16:05:45 +0200

    feat(password): knows that a password is valid if it is between 5 and 15 characters

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,5 +1,11 @@
 export class PasswordValidator{
     static validate(password: string) {
+        if (password.length >= 5 && password.length <= 15) {
+            return {
+                isValid: true,
+                errors: []
+            }
+        }
         return {
             isValid: false,
             errors: ["Password must be at least 5 characters long"]
